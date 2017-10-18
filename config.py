86ac861591303f415d600eb1678211016155ab53
Alfred Zhou <adaijiushiwo@gmail.com>
2017-10-18 16:41:36 +0800

    feat:

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
-CLOC_LABEL = "Github"
-CODE_PATH = "/data/github/"
+CLOC_LABEL = "Gitlab"
+CODE_PATH = "/data/gitlab/git-data"
 CODE_EXPORT = "/data/code_export/"
 JSON_REPORT = "{0}{1}.json".format(CODE_EXPORT,CLOC_LABEL)
 CLOC_RUN_PATH = "/data/cloc-1.66.pl"
