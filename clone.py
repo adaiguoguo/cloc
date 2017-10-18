f29f443221b46fdb5f845b2e57f7d42dac21b8c1
Alfred Zhou <adaijiushiwo@gmail.com>
2017-10-18 16:47:28 +0800

    feat:

--- clone.py
+++ clone.py
@@ -4,7 +4,6 @@ import config
 import glob
 import shutil
 def git_clone(depository):
-    os.mkdir(config.CODE_EXPORT)
     if depository == "Github":
         newestfolder = max(glob.iglob("%s*"%(config.CODE_PATH)), key=os.path.getctime)
         os.chdir(config.CODE_EXPORT)
